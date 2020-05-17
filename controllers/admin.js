bfdf3090195e2dfc1d631396b0f02db0de03c880
sabit <iamsabit99@gmail.com>
2020-05-17 23:35:26 +0600

    user dashbord data show done

--- controllers/admin.js
+++ controllers/admin.js
@@ -2,8 +2,27 @@ var mysql = require('mysql');
 
 // login get request
 exports.getLogin = (req,res,next)=>{
-
-    res.render('admin/login',{msg:"", err: ""});
+    if(req.session.admin == undefined){
+        res.render('admin/login',{msg:"", err: ""});
+    }
+    else{
+        var connectDB = mysql.createConnection({
+            host: "localhost",
+            user: "root",
+            password: "",
+            database: "hotel"
+         });
+         data1 = "SELECT * "+
+            "FROM  bookingstatus "+
+            "WHERE status = 0 "; 
+        connectDB.query(data1,(err1,result1)=>{
+                if(err1) throw err1;
+                else{
+                    return res.render('admin/index',{msg:"", err: "",data: result1});
+                }
+         })
+    }
+   
 }
 
 //login post request
@@ -21,12 +40,22 @@ exports.postLogin = (req,res,next)=>{
             "WHERE name = "+mysql.escape(req.body.name) +
             "AND pass = "+ mysql.escape(req.body.pass);
 
+    data1 = "SELECT * "+
+            "FROM  bookingstatus "+
+            "WHERE status = 0 ";       
+
      connectDB.query(data,(err,result)=>{
          if(err) throw err;
          else{
              if(result.length){
                  req.session.admin =  result[0].name;
-                return res.render('admin/index',{msg:"", err: ""});
+                 connectDB.query(data1,(err1,result1)=>{
+                        if(err1) throw err1;
+                        else{
+                            return res.render('admin/index',{msg:"", err: "",data: result1});
+                        }
+                 })
+                
              }
              else{
                 return res.render('admin/login',{msg:"", err: "Please Check Your Information Again"});
